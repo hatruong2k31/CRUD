8f225eae230f95c8bb5b50401d11f54c92ad5381
hqtruong <hatruong2k31@gmail.com>
2022-07-13 22:22:45 +0700

    update 13/07

--- routes/login.js
+++ routes/login.js
@@ -20,37 +20,58 @@ app.get("/login", function (req, res, next) {
   });
 });
 
+// Authenticate admin
+app.post("/auth", function (req, res, next) {
+  req.assert("admin", "Admin is required").notEmpty();
+  req.assert("password", "Password is required").notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (!errors) {
+    var admin = req.body.admin;
+    var password = req.body.password;
+
+    req.getConnection(function (error, conn) {
+      conn.query(
+        "SELECT * FROM admin WHERE admin = ? AND password = ?",
+        [admin, password],
+        function (err, rows, fields) {
+          if (err) throw err;
+
+          // if admin not found
+          if (rows.length <= 0) {
+            req.flash("error", "Please correct enter Admin and Password!");
+            res.redirect("/login");
+          } else {
+            // if admin found
+            // render to views/home.js template file
+            req.session.loggedin = true;
+            req.session.admin = admin;
+            res.redirect("/home");
+          }
+        }
+      );
+    });
+  } else {
+    //Display errors to user
+    var error_msg = "";
+    errors.forEach(function (error) {
+      error_msg += error.msg + "<br>";
+    });
+    req.flash("error", error_msg);
+    res.render("login/login/", {
+      title: "",
+      admin: req.body.admin,
+      password: req.body.password,
+    });
+  }
+});
+
 // Show Logout
 app.get("/logout", function (req, res) {
   req.session.destroy();
   req.flash("success", "Login Again Here");
   res.redirect("/login");
-});
-
-// Authenticate admin
-app.post("/auth", function (req, res, next) {
-  var admin = req.body.admin;
-  var password = req.body.password;
-
-  conn.query(
-    "SELECT * FROM admin WHERE admin = ? AND password = ?",
-    [admin, password],
-    function (err, rows, fields) {
-      if (err) throw err;
-
-      // if admin not found
-      if (rows.length <= 0) {
-        req.flash("error", "Please correct enter Admin and Password!");
-        res.redirect("/login");
-      } else {
-        // if admin found
-        // render to views/home.js template file
-        req.session.loggedin = true;
-        req.session.admin = admin;
-        res.redirect("/home");
-      }
-    }
-  );
 });
 
 //Show home page if login succes
